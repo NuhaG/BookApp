9c7c3eec21ade2bcb36fdf996796bb6aa80ffa44
NuhaG <nuhaghoajria@gmail.com>
2026-05-31 19:32:01 +0530

    add frontend origin

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,12 @@ const PORT = process.env.PORT || 5555;
 
 // middlewares
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://book-verse-azure.vercel.app/",
+    credentials: true,
+  }),
+);
 // Serve locally stored uploads.
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
